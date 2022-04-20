99fb3195d9849594446d2ec6aa6e430db2067609
zealrussell <1057021029@qq.com>
2022-04-20 15:00:25 +0800

    AOP

---
+++ src/main/java/com/zeal/store/aop/TimerAspect.java
@@ -0,0 +1,34 @@
+package com.zeal.store.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * WHAT THE ZZZZEAL
+ *
+ * @author zeal
+ * @version 1.0
+ * @date 2022/4/20 13:50
+ */
+
+@Aspect
+@Component
+public class TimerAspect {
+
+    @Around("execution(* com.zeal.store.service.impl.*.*(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        // 记录起始时间
+        long start = System.currentTimeMillis();
+        // 执行连接点方法，即切面所在位置对应的方法。本项目中表示执行注册或执行登录等
+        Object result = pjp.proceed();
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        // 计算耗时
+        System.err.println(pjp.getTarget().getClass().getName() + "耗时：" + (end - start) + "ms.");
+        // 返回连接点方法的返回值
+        return result;
+    }
+
+}
